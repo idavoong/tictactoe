41b23367f588039b75aeae7596fb8f5562acd389
Glitch (tic-tac-toefds) <none>
2022-02-18 20:41:33 +0000

    🐊🗞️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,43 +1,71 @@
+var player = "X";
+var turns = 0;
+var end = false;
 
 
+function performLogic(button, tile) { 
+  if (player === "X"){
+    $(tile).text('X');
+  } else {
+    $(tile).text('O');
+  }
+}
 
+function switchPlayer() {
+  if (player === "X"){
+    player = "O";
+  } else {
+    player = "X";
+  }
+}
 
-
+function checkHorizontal() {
+  
+}
 
 
 $("#button1").click(function() {
-    performLogic("#button1","#tile1");
+  performLogic("#button1","#tile1");
+  switchPlayer();
 });
 
 $("#button2").click(function() {
-    performLogic("#button2","#tile2");
+  performLogic("#button2","#tile2");
+  switchPlayer();
 });
 
 $("#button3").click(function() {
-    performLogic("#button3","#tile3");
+  performLogic("#button3","#tile3");
+  switchPlayer();
 });
 
 $("#button4").click(function() {
-    performLogic("#button4","#tile4");
+  performLogic("#button4","#tile4");
+  switchPlayer();
 });
 
 $("#button5").click(function() {
-    performLogic("#button5","#tile5");
+  performLogic("#button5","#tile5");
+  switchPlayer();
 });
 
 $("#button6").click(function() {
-    performLogic("#button6","#tile6");
+  performLogic("#button6","#tile6");
+  switchPlayer();
 });
 
 $("#button7").click(function() {
-    performLogic("#button7","#tile7");
+  performLogic("#button7","#tile7");
+  switchPlayer();
 });
 
 $("#button8").click(function() {
-    performLogic("#button8","#tile8");
+  performLogic("#button8","#tile8");
+  switchPlayer();
 });
 
 $("#button9").click(function() {
-    performLogic("#button9","#tile9");
+  performLogic("#button9","#tile9");
+  switchPlayer();
 });
 
